1423fac1cbc77407cf5b2ca6e2ae683f3764398a
rodrigo <rodrigorivasmiranda@gmail.com>
2021-07-01 12:21:04 -0500

    actualizado

---
+++ Conversion_de_datos.java
@@ -0,0 +1,19 @@
+public class Conversion_de_datos{
+    public static void main(String[] args) {
+        /* 
+ * Tipos de datos Java
+ 
+ * Numericos: byte, short, int, log, float(numeros decimales), double
+ * Booleanos: (verdadero, falso) : boolean
+ * Caracteres: char
+ * Cadenas: String
+ */
+       int numero1=1000;
+       long numero2=numero1;
+       // int numero4=numero2; //puedo convertir de int a lon pero no long a int
+       float numero3=numero2;
+       System.out.println(numero1);
+       System.out.println(numero2);
+       System.out.println(numero3);
+    }
+}
